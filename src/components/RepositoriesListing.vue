b27fdee130eb23ce218856b2a2af76c46f346d24
ouhoy <kimkinon@gmail.com>
2023-09-21 22:21:08 +0100

    List repositories

---
+++ src/components/RepositoriesListing.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+
+export default {
+  props: {"repositories": Array}
+
+}
+
+</script>
+
+<template>
+  <div class="repo-container">
+    <div class="jobs-table relative overflow-x-auto shadow sm:rounded-lg">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th scope="col" class="px-6 py-3">
+            ID
+          </th>
+
+          <th scope="col" class="px-6 py-3">
+            Title
+          </th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr v-for="repo in repositories" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+
+          <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            {{ repo.id }}
+          </th>
+                <td class="px-6 py-4">
+          {{ repo.name }}
+        </td>
+        </tr>
+
+        </tbody>
+      </table>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.repo-container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
--- src/components/UserAuth.vue
+++ src/components/UserAuth.vue
@@ -7,7 +7,7 @@
 
 
         <input id="email-address" type="text" v-model="userName"
-               class="w-80 min-w-0 flex-auto rounded-md border-2 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+               class="w-80 min-w-0  text-gray-800 flex-auto rounded-md border-2 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
 
                placeholder="Enter your github username"/>
 
@@ -25,25 +25,42 @@
     </form>
 
   </div>
+  <RepositoriesListing v-if="repositories.length" :repositories="repositories"></RepositoriesListing>
+
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue'
+import axios from "axios";
+import RepositoriesListing from "@/components/RepositoriesListing.vue";
 
 export default defineComponent({
+  components: {RepositoriesListing},
 
   data() {
     return {
       userName: "",
-      userNameError: ""
+      userNameError: "",
+      repositories: []
     }
   },
   methods: {
     handleSubmit() {
-      this.userNameError = !this.userName ? "User name is required" : ""
 
+      this.userNameError = !this.userName ? "User name is required" : ""
+      axios
+          .get(`https://api.github.com/users/${this.userName}/repos`)
+          .then((response) => {
+            this.repositories = response.data;
+          })
+          .catch((error) => {
+            this.userNameError = !this.userName ? "Username is required" : "Username does not exist"
+            console.error("Error: ", error)
+          });
     }
-  }
+  },
+
+
 })
 
 </script>
